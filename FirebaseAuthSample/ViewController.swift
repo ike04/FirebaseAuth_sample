e4927e57bb17382b8e889a84b8507c5aa00f663b
ike04 <boc.0409@outlook.jp>
2020-11-28 13:05:54 +0900

    first commit

---
+++ FirebaseAuthSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseAuthSample
+//
+//  Created by 池田一成 on 2020/11/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
